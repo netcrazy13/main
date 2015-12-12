a92d46065bdbc71ae4e8098b9587e9cc76b462fd
netcrazy13 <netcrazy.n@gmail.com>
2015-12-12 17:43:32 +0700

    testmain

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main (){
+	
+	int x,y,sum;
+ printf("Enter The Length is : ");
+  scanf ("%d",&x);
+    printf("Enter The Width is : ");
+   scanf ("%d",&y);
+  sum = x*y;
+ printf("The area is :%d",sum);
+    getch();
+	
+
+}
+
